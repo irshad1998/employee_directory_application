144540e04651bce6ba512ed0447dbf03a3336ccc
Irshad KP <69163301+irshad1998@users.noreply.github.com>
2022-10-23 18:44:05 -0700

    Network client

--- lib/app/data/strings.dart
+++ lib/app/data/strings.dart
@@ -2,4 +2,13 @@ class Strings {
   Strings._();
 
   static const String searchHintText = 'Search employee by name or email';
+  static const String defaultLoadingMessage = 'Please wait...';
+  static const String httpMethodGet = 'GET';
+  static const String somethingWentWrong = 'Something wen\'t wrong';
+  static const String otherError = 'Other errors!';
+  static const String connectionTimeout = 'Connection timed out!';
+  static const String requestTimeout = 'Request timed out!';
+  static const String responseTimeout = 'Response timed out!';
+  static const String serviceException = 'Service exception!';
+  static const String requestCancellation = 'request cancellation!';
 }
