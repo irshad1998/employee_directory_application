5a9430c91891cd0cf6945e13ec208c70d0314b26
Irshad KP <69163301+irshad1998@users.noreply.github.com>
2022-10-23 18:14:58 -0700

    Added employee models

---
+++ lib/app/modules/home/repositories/i_employee_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:employee_directory_application/app/core/failures/api_failure.dart';
+import 'package:employee_directory_application/app/modules/home/models/employee.dart';
+
+abstract class IEmployeeRepo {
+  Future<Either<ApiFailure, List<Employee>>> getEmployeeList();
+}
